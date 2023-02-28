6c0668cb8f5aaf170f49c87cf31c5fe5d67d144b
Ali Neaz <neazali77@gmail.com>
2023-02-28 08:13:45 -0500

    add list and list-methods

---
+++ 06_day6/02_list.py
@@ -0,0 +1,22 @@
+
+
+ls = [1,2,3,4,5]
+print(ls[0])
+print(ls)
+print(ls[0:3])
+print(ls[::2])
+print(ls[1::2])
+# print(ls[100])
+
+#* negative indexing
+print(ls[-1])
+print(ls[-3:-1])
+
+print(len(ls))
+
+#* chenging values in a list
+print(ls)
+ls[2]=12014
+print(ls)
+
+#---------------
