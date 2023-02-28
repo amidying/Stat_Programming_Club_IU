320d64efab4383150f597d029e682c1e7d18d547
Ali Neaz <neazali77@gmail.com>
2023-02-28 06:19:45 -0500

    add typecasting practice

---
+++ 06_day6/01_practice.py
@@ -0,0 +1,20 @@
+# * Type casting
+
+# prbl1
+a = input("Enter num 1: ")
+b = input("Enter num 2: ")
+a = float(a)
+b = float(b)
+print(a+b)
+
+a = float(input("Enter num 1: "))
+b = float(input("Enter num 2: "))
+print(a+b)
+
+a = input("Enter num 1: ")
+b = input("Enter num 2: ")
+print(type(a))
+print(type(b))
+print(float(a)+float(b))
+print(type(a))
+print(type(b))
