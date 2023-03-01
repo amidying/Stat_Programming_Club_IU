11a00b7614b6935beb756b599ce96950333d1509
Ali Neaz <neazali77@gmail.com>
2023-03-01 07:03:56 -0500

    add list practice

---
+++ 06_day6/list_practice.py
@@ -0,0 +1,22 @@
+from math import sqrt
+
+ls = [1,2,3,4,5]
+print(ls)
+ls.pop()
+print(ls)
+ls.pop(2)
+print(ls)
+
+
+# x= (-b+-sqrt(b^2-4ac))/2a
+
+a = 5
+b = 2
+c = -10
+x1 = (-b+sqrt(b**2-4*a*c))/(2*a)
+x2 = (-b-sqrt(b**2-4*a*c))/(2*a)
+print(x1,x2)
+
+km = 100
+m = km*100
+print(km,"km is = ",m,"meter")
