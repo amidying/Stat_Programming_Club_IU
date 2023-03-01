e5b166a332c8ff175f5e0a53b62029d94fa15436
Ali Neaz <neazali77@gmail.com>
2023-03-01 13:53:24 -0500

    nothing new

---
+++ 06_day6/extra.py
@@ -0,0 +1,32 @@
+# x = 20
+# y = 40
+# # x,y = y,x
+
+# print("X value is: ",x)
+# print("Y value is: ",y)
+
+# # x,y = 20,40
+# # x,y = y,x
+# # x = y
+# # y = x
+# print(x,y)
+
+# z = x
+# x = y
+# y = z
+# print(x,y)
+
+myname = "a"
+yourname = "b"
+temp = myname
+myname = yourname
+yourname = temp
+print(myname,yourname)
+
+x = None
+y = False
+temp = x
+x = y
+y = temp
+print(x,y)
+
--- 06_day6/list_practice.py
+++ 06_day6/list_practice.py
@@ -10,12 +10,18 @@ print(ls)
 
 # x= (-b+-sqrt(b^2-4ac))/2a
 
-a = 5
-b = 2
-c = -10
+# a = 5
+# b = 2
+# c = -10
+a = float(input("Enter a:"))
+b = float(input("Enter b"))
+c = float(input("Enter c: "))
+
 x1 = (-b+sqrt(b**2-4*a*c))/(2*a)
 x2 = (-b-sqrt(b**2-4*a*c))/(2*a)
 print(x1,x2)
+print("Value error.")
+
 
 km = 100
 m = km*100
