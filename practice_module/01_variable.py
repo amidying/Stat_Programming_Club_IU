e8d67a002caa2a69ec0144e875567014778a7ae9
Ali Neaz <neazali77@gmail.com>
2023-03-04 05:51:21 -0500

    added practice folder

---
+++ practice_module/01_variable.py
@@ -0,0 +1,22 @@
+
+
+print("hello world")
+
+num = 15
+num2 = 23.23232
+name = "Name"
+Run = False
+N = None
+print(num)
+print(num2)
+
+print("Printing the type of all the variables.")
+print(type(num))
+print(type(num2))
+# ---------------------------- Rules for variable naming -----------
+
+var  = 5644
+is_valid_num = True
+# 6num = 545
+# is valid num = False
+_is_valid_num = True
