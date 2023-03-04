5fde153ebee7d12760815e790befe061af0ef4b4
Ali Neaz <neazali77@gmail.com>
2023-03-04 06:06:03 -0500

    add data types and operators

---
+++ practice_module/02_data_types.py
@@ -0,0 +1,48 @@
+
+#* Arithmatic Operator
+num1 = 154
+num2 = 45
+total = num1+num2
+print(total)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2) # floor division
+print(num1**num2) # power
+print(num1%num2) # modulous
+
+# Assignment operator
+total = num1+num2
+'''
+x = 50
+x += 5
+Flow of Programm: From left to right and up to bottom
+'''
+x = 50
+x += 5 # x = x+5
+print(x)
+
+# Comparison Operator
+a = 11
+b = 45
+print(a>b)
+print(a==b)
+print(a < b)
+print( a <= b)
+print(a >= b)
+print(a != b)
+
+print("-----------------------------------------------------")
+# Logical Operator
+# and or not
+
+a = True
+b = True
+c = False
+print(a and b)
+print(a and c)
+print(a or b)
+print(a or c)
+# not works on single operand
+print(not a)
+print(not c)
