8f9100927e6db84ec403f3565ba3298ea5648cff
Ali Neaz <neazali77@gmail.com>
2023-03-05 08:07:46 -0500

    add if else condition

---
+++ 09_day9/01_if_else.py
@@ -0,0 +1,13 @@
+
+a = 25
+b = 12
+#a,b=b,a variable swapping
+if a>0:
+    print("a is positive")
+else:
+    print("a is negative")
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is less than b")
---
+++ 09_day9/02_ifelse.py
@@ -0,0 +1,31 @@
+
+a = 12
+b = 33
+c = 8
+
+if a>b and a >c:
+    print("a is the greatest")
+
+if True or False:
+    print("True")
+
+if a>b: # nested
+    if a>c:
+        print("a is the greatest")
+
+# age between 0 to n
+age = 13
+if age>0 and age <=12:
+    print("baby")
+elif age >12 and age <=19:
+    print("Teen")
+else:
+    print("adult")
+
+if a>=0:
+    print("positive")
+elif a <0:
+    print("negative")
+
+
+
