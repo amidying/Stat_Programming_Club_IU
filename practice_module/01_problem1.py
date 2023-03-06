05bc521748d5fe3cb5275076e59140d63ad24a29
Ali Neaz <neazali77@gmail.com>
2023-03-06 06:25:42 -0500

    add problem 1 of conditional expression

---
+++ practice_module/01_problem1.py
@@ -0,0 +1,25 @@
+
+
+a = 3
+b = 2
+c = 1
+d = 15
+
+e = max(a,b)
+f = max(c,d)
+print(max(e,f))
+
+if a>b:
+    a = a
+else:
+    a = b
+if c > d:
+    c = c
+else:
+    c = d
+
+if a > c:
+    a = a
+else:
+    a = c
+print(a)
