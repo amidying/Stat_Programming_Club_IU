adce5545fdc0fb8be72dae6b7321cb7e3f619ac9
Ali Neaz <neazali77@gmail.com>
2023-03-07 01:59:45 -0500

    add started loop

---
+++ 10_day10/01_while.py
@@ -0,0 +1,24 @@
+
+
+# while conditin:
+#     code to run
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+'''
+run = True
+while run:
+    num = int(input("Enter a number: "))
+    print(num)
+    if num == 0:
+        run = False
+'''
+# print from 1 to 1000 using while loop
+
+i = 1
+while i<=1000:
+    print(i)
+    i += 1
+j = 1
