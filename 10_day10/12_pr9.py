f23eac8a0028d21cc599bfbf2d89bd960e1c7d48
Ali Neaz <neazali77@gmail.com>
2023-03-14 05:51:28 -0500

    add project 1

---
+++ 10_day10/12_pr9.py
@@ -0,0 +1,11 @@
+
+
+for i in range(3):
+    for j in range(3):
+        if i==1 and  j==1:
+            print("   ",end="")
+        else:
+            print(" * ",end="")
+    print()
+
+
---
+++ 11_day11_project1/01_rock_paper_scissor.py
@@ -0,0 +1,21 @@
+import random
+
+option = ['r','p','s']
+
+comp = random.choice(option)
+
+my_choice = input("Enter your choice: ")
+my_choice = my_choice.lower()
+
+if my_choice == comp:
+    print("Draw")
+elif my_choice == 'r' and comp == 's':
+    print("You win")
+elif my_choice == 's' and comp == 'p':
+    print("You win")
+elif my_choice == 'p' and comp == 'r':
+    print("You win")
+else:
+    print("You lose.")
+
+print(f"You choose: {my_choice} and computer choosed: {comp}")
