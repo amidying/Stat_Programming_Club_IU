164ad8e7ac233a4fa9a45290dd20818bc52e4cfe
Ali Neaz <neazali77@gmail.com>
2023-03-21 06:59:13 -0500

    add updated

--- 13_day14/07_pr8.py
+++ 13_day14/07_pr8.py
@@ -1,9 +1,10 @@
 
 
-n = 7 # take it as an input
+# n = 7 # take it as an input
+n = int(input("Enter you num: "))
 
 def multiplicationTable(n):
     for i in range(1,11):
-        print(f"{n}X{i}={n+i}")
+        print(f"{n}X{i}={n*i}")
 
 multiplicationTable(n)
