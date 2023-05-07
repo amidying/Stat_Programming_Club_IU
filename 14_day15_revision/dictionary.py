9cc32219f8ba8963f9a9d781fd2db1d2ca7ada1a
Ali Neaz <neazali77@gmail.com>
2023-05-07 19:33:29 +0600

    add dictionary problem

---
+++ 14_day15_revision/dictionary.py
@@ -0,0 +1,27 @@
+
+# 1
+'''
+words = {
+    "Aam" : "Mango",
+    "Kola" : "Banana",
+    "Angur" : "Grapes"
+}
+print("Availabe options: ")
+print(words.keys())
+word = input("Enter your bangla word: ")
+
+print(words[word])
+'''
+# 2
+'''
+S = set()
+for i in range(8):
+    num = int(input("Enter your number: "))
+    S.add(num)
+
+print(S)
+'''
+# 3
+
+S = {18,"18"}
+print(S)
