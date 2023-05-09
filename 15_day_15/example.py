46e32646a5aafbc1943e08e35edd5cbf3a2594e6
Ali Neaz <neazali77@gmail.com>
2023-05-09 18:51:02 +0600

    add day 15

---
+++ 15_day_15/example.py
@@ -0,0 +1,13 @@
+
+num = int(input("Enter: "))
+
+for i in range(num):
+    if i == 5:
+        continue
+    if i == 6:
+        print(i)
+        break
+    print(i)
+
+for i in range(10):
+    pass
